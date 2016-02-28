260f3c45ca1a1ba10affe38ff0d0e469d89c2164
oxyflour <oxyflour@hotmail.com>
2016-02-28 08:35:19 +0800

    add macros

--- lib/index.lisp
+++ lib/index.lisp
@@ -1,8 +1,13 @@
-letrec
-	printList (\ a
-		(if (list? a) {
-			(printList (head a))
-			(echo (tail a))
-		} 0)
-	)
-	(printList [1 2 3 4])
+macro
+	(and a b) (let v a (if v b v))
+	(or  a b) (let v a (if v v b))
+	(or (echo 1) (echo 2))
+;letrec
+;	(printList a)
+;		(if (list? a)
+;			{
+;				(printList (head a))
+;				(echo (tail a))
+;			}
+;			0)
+;	(printList [1 2 3 4])
--- src/parser.ts
+++ src/parser.ts
@@ -3,40 +3,71 @@ import { AstNode, Literal, Id, Lambda, Apply } from './common'
 const INFIX = '+|-|*|/|>|<|>=|<=|==|!=|,|;'
     .split('|').reduce((d, c) => (d[c] = 1, d), { })
 
-const SEPS = /(\(|\)|\[|\]|{|}|,|;)/g
+const SEPS = /(\(|\)|\[|\]|{|})/g
 
-function unfold(node: any): AstNode {
+function unfold(node: any, macros): AstNode {
     if (Array.isArray(node)) {
         var head = node[0]
         if (head === '\\') {
             var body = node.length > 3 ? [head].concat(node.slice(2)) : node[2]
-            return new Lambda(new Id(node[1]), unfold(body))
+            return new Lambda(new Id(node[1]), unfold(body, macros))
+        }
+        else if (head === 'macro') {
+            var body = node.length > 4 ? [head].concat(node.slice(3)) : node[3],
+                id = node[1], value = node[2]
+            if (Array.isArray(id)) {
+                value = [id.slice(1), value]
+                id = id[0]
+            }
+            macros = Object.assign({ }, macros, { [id]:value })
+            return unfold(body, macros)
+        }
+        else if (macros[head]) {
+            var [args, body] = macros[head],
+                vals = node.slice(1)
+            macros = Object.assign({ }, macros)
+            args.forEach((n, i) => macros[n] = vals[i])
+            return unfold(body, macros)
         }
         else if (head === 'let') {
-            var body = node.length > 4 ? [head].concat(node.slice(3)) : node[3]
+            var body = node.length > 4 ? [head].concat(node.slice(3)) : node[3],
+                variable = node[1], value = node[2]
+            if (Array.isArray(variable)) {
+                value = ['\\'].concat(variable.slice(1)).concat([node[2]])
+                variable = variable[0]
+            }
             return unfold([
-                ['\\', node[1], body],
-                node[2]])
+                ['\\', variable, body],
+                value
+            ], macros)
         }
         else if (head === 'letrec') {
-            var body = node.length > 4 ? [head].concat(node.slice(3)) : node[3]
+            var body = node.length > 4 ? [head].concat(node.slice(3)) : node[3],
+                variable = node[1], value = node[2]
+            if (Array.isArray(variable)) {
+                value = ['\\'].concat(variable.slice(1)).concat([node[2]])
+                variable = variable[0]
+            }
             return unfold([
-                ['\\', node[1], body],
-                ['Y', ['\\', node[1], node[2]]]])
+                ['\\', variable, body],
+                ['Y', ['\\', variable, value]]
+            ], macros)
         }
         else if (head === 'if') {
             var body = node.length > 4 ? [head].concat(node.slice(3)) : node[3]
             return unfold([
                 ['?', node[1],
                     ['\\', '_', node[2]],
-                    ['\\', '_', body]],
-                '0'])
+                    ['\\', '_', body]
+                ],
+                '0',
+            ], macros)
         }
         else {
             var func = node.length > 2 ? node.slice(0, -1) : node[0],
                 arg = node[node.length - 1]
             if (INFIX[ arg ]) [func, arg] = [arg, func]
-            return new Apply(unfold(func), unfold(arg))
+            return new Apply(unfold(func, macros), unfold(arg, macros))
         }
     }
     else {
@@ -45,6 +76,8 @@ function unfold(node: any): AstNode {
             return new Literal(+node)
         else if (node === 'true' || node === 'false')
             return new Literal(node === 'true')
+        else if (macros[node])
+            return unfold(macros[node], macros)
         else
             return new Id(node)
     }
@@ -61,7 +94,7 @@ function convertNode(node, token) {
 }
 
 export function parse(source: string): AstNode {
-    var tokens = source.replace(SEPS, ' $1 ')
+    var tokens = source.replace(SEPS, ' $1 ').replace(/;[^\n]*/g, '')
             .replace(/\s+/g, ' ').split(' ').filter(x => x.length > 0),
         stack = [ [] ]
     tokens.forEach(token => {
@@ -72,5 +105,5 @@ export function parse(source: string): AstNode {
         else
             stack[stack.length - 1].push(token)
     })
-    return unfold(stack[0])
+    return unfold(stack[0], { })
 }
