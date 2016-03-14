297609e917cabba14e36ac6c5da3e154a7a5c167
oxyflour <oxyflour@hotmail.com>
2016-03-14 21:42:08 +0800

    remove broken vm

--- index.ts
+++ index.ts
@@ -6,25 +6,6 @@ import { parse } from './src/parser'
 
 import { values, types, compilePrelude, compileVarRemap } from './src/buildins'
 
-function runVM(exp, values) {
-    var stack: [AstNode, any, boolean][] = [ [exp, values, true] ],
-        func = null
-
-    function step() {
-        var [node, env, isFunc] = stack.pop()
-        if (node instanceof Apply)
-            stack.push([node.arg, env, false], [node.func, env, true])
-        else if (isFunc)
-            func = node.evaluate(env)
-        else
-            func = Apply.evaluate(func, node.evaluate(env), '')
-    }
-
-    while (stack.length)
-        step()
-    return func
-}
-
 declare function require(module: string): any
 
 try {
@@ -32,10 +13,9 @@ try {
         type = exp.analyse(new TypeEnv(types as any), new Set()),
         src = exp.compile(compileVarRemap)
 
-    var val1 = runVM(exp, values),
-        val2 = exp.evaluate(values),
+    var val2 = exp.evaluate(values),
         val3 = eval(compilePrelude + src)
-    console.log(val1, val2, val3)
+    console.log(val2, val3)
     console.log(src)
 }
 catch (e) {
